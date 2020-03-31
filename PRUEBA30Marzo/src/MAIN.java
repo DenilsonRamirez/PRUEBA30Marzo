c487f790380f60652bbca05befd56de6350cfdc0
Jan Carlo De León <62573884+jancarlo-deleon@users.noreply.github.com>
2020-03-30 19:07:51 -0600

    Update MAIN.java
    Adición de datos personales.

--- PRUEBA30Marzo/src/MAIN.java
+++ PRUEBA30Marzo/src/MAIN.java
@@ -5,5 +5,11 @@ public class MAIN {
        System.out.println("Nombre: Denilson Baltazar Ramírez Andrés");
        System.out.println("Direccion: Barillas Zona 6");
        System.out.println("Telefono: 46400006");
+        
+        
+        System.out.println("JAN CARLO DE LEÓN CIFUENTES");
+        System.out.println("11 CALLE 2-43 ZONA 5 COLONIA LOS ENCINOS");
+        System.out.println("33361433");
+        
     }
 }
