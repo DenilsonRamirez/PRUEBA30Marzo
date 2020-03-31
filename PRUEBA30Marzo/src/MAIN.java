0778e14392cbf7755578be5411e56cab83e94d99
DenilsonRamirez <denixmas123@gmail.com>
2020-03-30 18:30:39 -0600

    Add files via upload

---
+++ PRUEBA30Marzo/src/MAIN.java
@@ -0,0 +1,9 @@
+
+public class MAIN {
+    
+    public static void main(String[] args) {
+       System.out.println("Nombre: Denilson Baltazar Ramírez Andrés");
+       System.out.println("Direccion: Barillas Zona 6");
+       System.out.println("Telefono: 46400006");
+    }
+}
